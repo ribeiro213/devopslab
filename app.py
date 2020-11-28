698f81f95bd797c32c1d5624be21f42dcda5514d
Ivan Ribeiro <ivanribeiro21@hotmail.com>
2020-11-28 18:18:36 +0000

    Bug Removido

--- app.py
+++ app.py
@@ -7,16 +7,6 @@ app = Flask(__name__)
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
-@app.route('/bug')
-def bad():
-    try:
-        raise TypeError()
-    except TypeError as e:
-        print(e)
-    except TypeError as e:
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
-
-
 if __name__ == '__main__':
     app.run(debug=True)
 
