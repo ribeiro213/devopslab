739b14ce03a674eae8399e84cd736d9f646a3c6d
Ivan Ribeiro <ivanribeiro21@hotmail.com>
2020-11-28 18:42:02 +0000

    CSRF

--- app.py
+++ app.py
@@ -3,18 +3,11 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
     return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
